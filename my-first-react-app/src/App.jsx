86b1e0f285d983eb93addf0114909cb384bec6f0
justinp24 <justinpardo2423@gmail.com>
2024-04-04 19:03:06 -0400

    lifted state for personal component into main app

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,21 +1,46 @@
-import SideMenu from "./components/sidemenu";
 import Personal from "./components/personal";
 import Education from "./components/education";
 import Experience from "./components/experience";
 import "./App.css";
 import "./styles/resume.css";
+import { useCallback, useState } from "react";
 function App() {
+    //personal
+    const [personalData, setPersonalData] = useState({
+        fullName: "",
+        email: "",
+        phone: "",
+        address: "",
+    });
+    const handlePersonChange = (e) => {
+        const { name, value } = e.target;
+        setPersonalData((prevdata) => ({
+            ...prevdata,
+            [name]: value,
+        }));
+    };
+
     return (
         <div className="app">
             <div className="App">
-                <SideMenu>
-                    <Personal></Personal>
-                    <Education></Education>
-                    <Experience></Experience>
-                </SideMenu>
+                <Personal
+                    personalData={personalData}
+                    handlePersonalChange={handlePersonChange}
+                ></Personal>
+                <Education></Education>
+                <Experience></Experience>
             </div>
             <div className="resume">
-                <h2>Resume</h2>
+                <h2>{personalData.fullName}</h2>
+                <ul>
+                    {Object.keys(personalData).map((key) => {
+                        if (key != "fullName") {
+                            return <p key={key}>{personalData[key]}</p>;
+                        } else {
+                            return null;
+                        }
+                    })}
+                </ul>
             </div>
         </div>
     );
--- my-first-react-app/src/components/personal.jsx
+++ my-first-react-app/src/components/personal.jsx
@@ -1,16 +1,40 @@
 import "../styles/personal.css";
-export default function Personal() {
+export default function Personal({ personalData, handlePersonalChange }) {
     return (
         <div className="personal">
             <h1>Personal Details</h1>
             <label htmlFor="fullName">FullName</label>
-            <input type="text" name="fullname " id="fullname" />
+            <input
+                type="text"
+                name="fullName"
+                id="fullName"
+                value={personalData.fullName}
+                onChange={handlePersonalChange}
+            />
             <label htmlFor="email">Email</label>
-            <input type="email" name="email" id="email" />
+            <input
+                type="email"
+                name="email"
+                id="email"
+                value={personalData.email}
+                onChange={handlePersonalChange}
+            />
             <label htmlFor="phone">Phone number</label>
-            <input type="tel" name="phone" id="phone" />
+            <input
+                type="tel"
+                name="phone"
+                id="phone"
+                value={personalData.phone}
+                onChange={handlePersonalChange}
+            />
             <label htmlFor="address">Address</label>
-            <input type="text" name="address" id="address" />
+            <input
+                type="text"
+                name="address"
+                id="address"
+                value={personalData.addres}
+                onChange={handlePersonalChange}
+            />
         </div>
     );
 }
