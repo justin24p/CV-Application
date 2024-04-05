6f7a41ccb722936ee00fc8210bb55175fc789647
justinp24 <justinpardo2423@gmail.com>
2024-04-05 19:27:31 -0400

    create functionality for both education & experience toggle with pairing form components

--- my-first-react-app/src/App.jsx
+++ my-first-react-app/src/App.jsx
@@ -1,8 +1,8 @@
 import Personal from "./components/personal";
-import Experience from "./components/experience";
 import Resume from "./components/resume";
 import { useState } from "react";
 import EducationInput from "./components/education";
+import ExperienceForm from "./components/experience";
 //css
 import "./App.css";
 import "./styles/resume.css";
@@ -23,6 +23,22 @@ function App() {
         endDate: "",
         area: "",
     });
+    // Experience Data
+    const [experienceData, setExperienceData] = useState({
+        companyName: "",
+        positionTitle: "",
+        startDate: "",
+        endDate: "",
+        area: "",
+        description: "",
+    });
+    const handleExperienceChange = (e) => {
+        const { name, value } = e.target;
+        setExperienceData((prevdata) => ({
+            ...prevdata,
+            [name]: value,
+        }));
+    };
     const handleEducationChange = (e) => {
         const { name, value } = e.target;
         setEducationData((prevdata) => ({
@@ -38,18 +54,27 @@ function App() {
             [name]: value,
         }));
     };
-    // Toggle handler for education could be refactored to not use state for button only form
-    const [showbutton, setShowButton] = useState(false);
+    // Toggle handler for education
+    // education button
+    const [showdiv, setShowDiv] = useState(false);
     const [showform, setShowForm] = useState(false);
-
-    const toggleButton = () => {
-        setShowButton(!showbutton);
+    //! converts from true to false
+    const toggleDiv = () => {
+        setShowDiv(!showdiv);
     };
-    const handeEducation = () => {
+    const handleEducation = () => {
         setShowForm(true);
-        setShowButton(false);
     };
+    // Toggle handler for experience
+    const [showdiv2, setShowDiv2] = useState(false);
+    const [showform2, setShowForm2] = useState(false);
 
+    const toggleDiv2 = () => {
+        setShowDiv2(!showdiv2);
+    };
+    const handleExperience = () => {
+        setShowForm2(true);
+    };
     return (
         <div className="app">
             <div className="App">
@@ -57,18 +82,46 @@ function App() {
                     personalData={personalData}
                     handlePersonalChange={handlePersonChange}
                 ></Personal>
-                <Experience></Experience>
+                <div className="experience">
+                    <h1>Experience</h1>
+                    <button onClick={toggleDiv2}>^</button>
+                    {showdiv2 && (
+                        <div>
+                            {!showform2 && (
+                                <button onClick={handleExperience}>
+                                    Education +
+                                </button>
+                            )}
+                            {showform2 && (
+                                <ExperienceForm
+                                    experienceData={experienceData}
+                                    handleExperienceChange={
+                                        handleExperienceChange
+                                    }
+                                ></ExperienceForm>
+                            )}
+                        </div>
+                    )}
+                </div>
                 <div className="education">
                     <h1>Education</h1>
-                    <button onClick={toggleButton}>^</button>
-                    {showbutton && (
-                        <button onClick={handeEducation}>Education +</button>
-                    )}
-                    {showform && (
-                        <EducationInput
-                            educationData={educationData}
-                            handleEducationChange={handleEducationChange}
-                        ></EducationInput>
+                    <button onClick={toggleDiv}>^</button>
+                    {showdiv && (
+                        <div>
+                            {!showform && (
+                                <button onClick={handleEducation}>
+                                    Education +
+                                </button>
+                            )}
+                            {showform && (
+                                <EducationInput
+                                    educationData={educationData}
+                                    handleEducationChange={
+                                        handleEducationChange
+                                    }
+                                ></EducationInput>
+                            )}
+                        </div>
                     )}
                 </div>
             </div>
--- my-first-react-app/src/components/experience.jsx
+++ my-first-react-app/src/components/experience.jsx
@@ -1,21 +1,57 @@
-import { useState } from "react";
-
-function Experience() {
-    const [showbutton, setShowButton] = useState(false);
-    const toggleButton = () => {
-        setShowButton(!showbutton);
-    };
-    const handeExperience = () => {
-        console.log("experience");
-    };
+export default function ExperienceForm({
+    experienceData,
+    handleExperienceChange,
+}) {
     return (
-        <>
-            <h1>Experience</h1>
-            <button onClick={toggleButton}>^</button>
-            {showbutton && (
-                <button onClick={handeExperience}>Experience + </button>
-            )}
-        </>
+        <div className="personal">
+            <label htmlFor="Company">Company Name</label>
+            <input
+                type="text"
+                name="Company"
+                id="Company"
+                value={experienceData.Company}
+                onChange={handleExperienceChange}
+            />
+            <label htmlFor="Position Title">Position Title</label>
+            <input
+                type="text"
+                name="positionTitle"
+                id="positionTitle"
+                value={experienceData.positionTitle}
+                onChange={handleExperienceChange}
+            />
+            <label htmlFor="startDate">startDate number</label>
+            <input
+                type="text"
+                name="startDate"
+                id="startDate"
+                value={experienceData.startDate}
+                onChange={handleExperienceChange}
+            />
+            <label htmlFor="endDate">endDate</label>
+            <input
+                type="text"
+                name="endDate"
+                id="endDate"
+                value={experienceData.endDate}
+                onChange={handleExperienceChange}
+            />
+            <label htmlFor="location">location</label>
+            <input
+                type="text"
+                name="location"
+                id="location"
+                value={experienceData.location}
+                onChange={handleExperienceChange}
+            />
+            <label htmlFor="description">Description</label>
+            <input
+                type="text"
+                name="description"
+                id="description"
+                value={experienceData.description}
+                onChange={handleExperienceChange}
+            />
+        </div>
     );
 }
-export default Experience;
